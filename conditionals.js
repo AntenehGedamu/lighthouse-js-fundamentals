39e2659b3e0dabee49c3e55ce96d99c2b95c5076
Anteneh Gedamu <gedamuanteneh@gmail.com>
2021-11-09 12:54:45 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -20,14 +20,33 @@
 //   console.log("Short sleeves are fine.");
 // }
 
-const temperature = 12;
+// const temperature = 12;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+// const isCitizen = true;
+// const age = 19;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+
+// let temperature = 41
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea…");
+// }
+
+let raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
-
-console.log("Now you're ready to go outside!");
